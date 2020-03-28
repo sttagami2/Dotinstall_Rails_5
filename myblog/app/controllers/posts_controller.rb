ca34821bd5fab3b363cb0277b6858bc01626c280
vagrant <vagrant@localhost.localdomain>
2020-03-28 11:15:32 +0000

    [Finish] lesson19 編集フォームを作ろう

--- myblog/app/controllers/posts_controller.rb
+++ myblog/app/controllers/posts_controller.rb
@@ -21,6 +21,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body)
