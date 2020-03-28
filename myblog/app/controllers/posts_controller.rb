1b153d984e1a144e9455a044184946dd7c16716d
vagrant <vagrant@localhost.localdomain>
2020-03-28 11:30:45 +0000

    [Finish] lesson20 記事の更新

--- myblog/app/controllers/posts_controller.rb
+++ myblog/app/controllers/posts_controller.rb
@@ -26,7 +26,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   private
